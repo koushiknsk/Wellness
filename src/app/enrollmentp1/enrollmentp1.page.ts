b24f03b64cf10d3cb99403fb400c8d6721cdd731
Gangumolu <naresh.gangumolu@capgemini.com>
2019-07-25 19:12:57 +0530

    splash enrollment

--- src/app/enrollmentp1/enrollmentp1.page.ts
+++ src/app/enrollmentp1/enrollmentp1.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-enrollmentp1',
@@ -9,7 +9,8 @@ export class Enrollmentp1Page implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
 
+  ngOnInit() {}
+    
+    
 }
