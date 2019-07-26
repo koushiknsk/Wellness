b5def787a3f77cbda6fa042d4015de242cf5e9b9
Nanduri <koushik.nanduri@capgemini.com>
2019-07-26 14:35:03 +0530

    Commiting rewards and mainlanding changes

---
+++ src/app/rewards-tab/rewards-tab.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rewards-tab',
+  templateUrl: './rewards-tab.page.html',
+  styleUrls: ['./rewards-tab.page.scss'],
+})
+export class RewardsTabPage implements OnInit {
+
+
+  constructor() {  
+    }
+
+    ngOnInit() {
+  
+    }
+
+}
