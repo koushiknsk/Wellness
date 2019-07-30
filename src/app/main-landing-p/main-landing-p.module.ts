afd5d318135712698f11042dedd512d347076fe8
Boyinapalli <sai.boyinapalli@capgemini.com>
2019-07-30 17:35:53 +0530

    circular_pgBar_home_page_changes

--- src/app/main-landing-p/main-landing-p.module.ts
+++ src/app/main-landing-p/main-landing-p.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+import { Component, OnInit} from '@angular/core';
 
 import { IonicModule } from '@ionic/angular';
 
 import { MainLandingPagePage } from './main-landing-p.page';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
+
 
 const routes: Routes = [
   {
@@ -14,13 +18,26 @@ const routes: Routes = [
   }
 ];
 
+@Component({
+  selector: 'app-main-landing-p',
+  templateUrl: './main-landing-p.page.html',
+  styleUrls: ['./main-landing-p.page.scss'],
+})
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),    
+    NgCircleProgressModule.forRoot({})
   ],
   declarations: [MainLandingPagePage]
 })
-export class MainLandingPagePageModule {}
+export class MainLandingPagePageModule implements OnInit{
+  ngOnInit() {    
+  }
+  constructor(){      
+  }
+      
+}
