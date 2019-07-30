52e9ba9b4feb2e2c49ea6fd33aa73e16990ce6ce
Boyinapalli <sai.boyinapalli@capgemini.com>
2019-07-30 19:35:42 +0530

    mlp_goals

--- src/app/main-landing-p/main-landing-p.page.ts
+++ src/app/main-landing-p/main-landing-p.page.ts
@@ -7,14 +7,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainLandingPagePage implements OnInit {
 
+  //Variables
+  isMainContentCardVisible: boolean = true;
+  isEarnMorePointsCardVisible: boolean = false;
+  isNutritionAssesmentCardVisible: boolean = false;
+  isSleepTrackingCardVisible: boolean = false;
+  isNutritionAssesDetailsPageVisible: boolean = false;
+  isNutritionCamPageVisible: boolean = false;
+  isFitnessDevicesPageVisible: boolean = false;
+
+
   constructor() {
   }
 
   ngOnInit() {
-    //this.setVisibility();
+
   }
 
-  earnMorePoints(){
-    
+  setEarnedPointsCardVisbility() {
+    this.isMainContentCardVisible = false;
+    this.isSleepTrackingCardVisible = false;
+    this.isNutritionAssesmentCardVisible = false;
+    this.isNutritionAssesDetailsPageVisible = false;
+    this.isNutritionCamPageVisible = false;
+    this.isFitnessDevicesPageVisible = false;
+    this.isEarnMorePointsCardVisible = true;
   }
+  setNutritionPageCardVisibility() {
+    this.isMainContentCardVisible = false;
+    this.isSleepTrackingCardVisible = false;
+    this.isNutritionAssesDetailsPageVisible = false;
+    this.isNutritionCamPageVisible = false;
+    this.isFitnessDevicesPageVisible = false;
+    this.isEarnMorePointsCardVisible = false;
+    this.isNutritionAssesmentCardVisible = true;
+  }
+  setSleepPageCardVisibility() {
+    this.isMainContentCardVisible = false;
+    this.isNutritionAssesDetailsPageVisible = false;
+    this.isNutritionCamPageVisible = false;
+    this.isFitnessDevicesPageVisible = false;
+    this.isEarnMorePointsCardVisible = false;
+    this.isNutritionAssesmentCardVisible = false;
+    this.isSleepTrackingCardVisible = true;
+  }
+  setNutritionAssesDetailsVisibility() {
+    this.isMainContentCardVisible = false;
+    this.isNutritionCamPageVisible = false;
+    this.isFitnessDevicesPageVisible = false;
+    this.isEarnMorePointsCardVisible = false;
+    this.isNutritionAssesmentCardVisible = false;
+    this.isSleepTrackingCardVisible = false;
+    this.isNutritionAssesDetailsPageVisible = true;
+  }
+  setNutritionCamPageVisibility() {
+    this.isMainContentCardVisible = false;
+    this.isFitnessDevicesPageVisible = false;
+    this.isEarnMorePointsCardVisible = false;
+    this.isNutritionAssesmentCardVisible = false;
+    this.isSleepTrackingCardVisible = false;
+    this.isNutritionAssesDetailsPageVisible = false;
+    this.isNutritionCamPageVisible = true;
+  }
+  setFitnessPageVisibility() {
+    this.isMainContentCardVisible = false;
+    this.isEarnMorePointsCardVisible = false;
+    this.isNutritionAssesmentCardVisible = false;
+    this.isSleepTrackingCardVisible = false;
+    this.isNutritionAssesDetailsPageVisible = false;
+    this.isNutritionCamPageVisible = false;
+    this.isFitnessDevicesPageVisible = true;
+
+  }
+
 }
