4ccf7596832b02726877b0ed883358f7bde44953
Aditya <gupta08aditya1996@gmail.com>
2019-08-05 00:20:23 +0530

    Weeky-Activity Screen

---
+++ src/app/weekly-report/weekly-report.page.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import {Chart} from 'chart.js'
+//import CanvasJS from 'canvasjs';
+import * as CanvasJS from 'canvasjs';
+
+
+@Component({
+  selector: 'app-weekly-report',
+  templateUrl: './weekly-report.page.html',
+  styleUrls: ['./weekly-report.page.scss'],
+})
+export class WeeklyReportPage implements OnInit {
+  BarChart=[];
+
+  constructor() { }
+
+  ngOnInit() {
+
+   // Bar chart:
+this.BarChart = new Chart('barChart', {
+  type: 'bar',
+data: {
+ labels: ["07th-13th July", "14th-19th July", "21st-27th July"],
+ datasets: [{
+     label: 'Sleep Activity',
+     data: [2000 ,8000 , 4000,10000],
+     backgroundColor: [
+         'rgba(54, 162, 235)',
+         'rgba(54, 162, 235)',
+         'rgba(54, 162, 235)'
+         
+     ],
+     borderColor: [
+         'rgba(54, 162, 235,1)',
+         'rgba(54, 162, 235, 1)',
+         'rgba(54, 162, 235, 1)'
+     ],
+     borderWidth: 0.5
+ }]
+}, 
+options: {
+ title:{
+     text:"Bar Chart",
+     display:false
+ },
+ scales: {
+     yAxes: [{
+         ticks: {
+             beginAtZero:true
+         }
+     }]
+ }
+}
+});
+
+  }
+
+}
